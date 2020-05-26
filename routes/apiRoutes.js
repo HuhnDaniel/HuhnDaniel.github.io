37d4ea728a140e974343a5d880fc1f8d352e41b8
Daniel Moody-Huhn <60518682+HuhnDaniel@users.noreply.github.com>
2020-05-26 12:38:57 -0500

    create and route portfolio api

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,3 +1,10 @@
 const messagesData = require("../db/messages.json");
+const portfolioData = require("../db/portfolio.json");
 const router = require("express").Router();
 const fs = require("fs");
+
+router.get("/portfolio", (req, res) => {
+    res.json(portfolioData);
+});
+
+module.exports = router;
