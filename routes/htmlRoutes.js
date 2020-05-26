678baa7a2f446e7fdf6e79d1251d45356c790a4b
Daniel Moody-Huhn <60518682+HuhnDaniel@users.noreply.github.com>
2020-05-26 00:33:41 -0500

    set up html router

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,2 +1,16 @@
 const router = require("express").Router();
 const path = require("path");
+
+router.get("/contact", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/contact.html"));
+});
+
+router.get("/portfolio", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/portfolio.html"));
+});
+
+router.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 
-const apiRoutes = require("./routes/apiRoutes");
+// const apiRoutes = require("./routes/apiRoutes");
 const htmlRoutes = require("./routes/htmlRoutes");
 
 const app = express();
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use("/api", apiRoutes);
+// app.use("/api", apiRoutes);
 app.use("", htmlRoutes);
 
 app.listen(PORT, function() {
