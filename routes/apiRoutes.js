b74315d3113d5e3388cf0ac9facc826483b5345a
Daniel Moody-Huhn <60518682+HuhnDaniel@users.noreply.github.com>
2020-06-30 13:34:28 -0500

    add about section

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,31 +1,3 @@
-const messagesData = require("../db/messages.json");
-const portfolioData = require("../db/portfolio.json");
 const router = require("express").Router();
-const fs = require("fs");
-
-router.get("/portfolio", (_, res) => {
-    res.json(portfolioData);
-});
-
-router.get("/messages", (_, res) => {
-    res.json(messagesData);
-})
-.post("/messages", (req, res) => {
-    messagesData.forEach(message => {
-        message.id++;
-    });
-
-    const newMessage = req.body;
-    newMessage.id = 1;
-    messagesData.unshift(newMessage);
-
-    fs.writeFile("./db/messages.json", JSON.stringify(messagesData, null, 2), err => {
-        if (err) throw err;
-
-        console.log("File saved");
-    });
-
-    res.end();
-});
 
 module.exports = router;
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,14 +1,6 @@
 const router = require("express").Router();
 const path = require("path");
 
-router.get("/contact", (_, res) => {
-    res.sendFile(path.join(__dirname, "../public/contact.html"));
-});
-
-router.get("/portfolio", (_, res) => {
-    res.sendFile(path.join(__dirname, "../public/portfolio.html"));
-});
-
 router.get("*", (_, res) => {
     res.sendFile(path.join(__dirname, "../public/index.html"));
 });
