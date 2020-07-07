fd5cffe171d686f3e727168279194ba8aa686fe7
Daniel Moody-Huhn <60518682+HuhnDaniel@users.noreply.github.com>
2020-07-07 14:41:45 -0500

    add projects screenshots/ column scrolling

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,15 +1,36 @@
 const menuBtn = $(".menu-toggle");
 const menu = $(".menu");
-menu.hide();
+const project = $(".project-logo");
 
 function menuToggle() {
     if (menuBtn.attr("data-status") === "closed") {
         menuBtn.attr("data-status", "open").text("✕");
-        menu.show(500);
+        menu.css({ display: "flex" });
     } else {
         menuBtn.attr("data-status", "closed").text("☰");
-        menu.hide(500);
+        menu.css({ display: "none" });
     };
 }
 
+function showScreenshot() {
+    this.src = `assets/images/${this.dataset.project}-screenshot.png`;
+}
+
+function revertScreenshot() {
+    this.src = `assets/images/${this.dataset.project}-logo.png`;
+}
+
+function checkWidth() {
+    if ($(window).width() >= 677 && $(window).width() < 1064) {
+        menuBtn.attr("data-status", "open").text("✕");
+        menu.css({ display: "flex" });
+    } else {
+        menuBtn.attr("data-status", "closed").text("☰");
+        menu.css({ display: "none" });
+    }
+}
+
 menuBtn.on("click", menuToggle);
+project.on("mouseover", showScreenshot);
+project.on("mouseout", revertScreenshot);
+$(window).on("resize", checkWidth);
