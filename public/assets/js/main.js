d4e3aa474c36c49dbf87eb37a948d582ade15ad0
Daniel Moody-Huhn <60518682+HuhnDaniel@users.noreply.github.com>
2020-07-06 19:57:18 -0500

    add togglable menu

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,10 +1,14 @@
 const menuBtn = $(".menu-toggle");
+const menu = $(".menu");
+menu.hide();
 
 function menuToggle() {
     if (menuBtn.attr("data-status") === "closed") {
         menuBtn.attr("data-status", "open").text("✕");
+        menu.show(500);
     } else {
         menuBtn.attr("data-status", "closed").text("☰");
+        menu.hide(500);
     };
 }
 
