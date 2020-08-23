8a2ccb55c880a07d474bb8b7198aeaeb9c56bc94
Daniel Moody-Huhn <60518682+HuhnDaniel@users.noreply.github.com>
2020-08-22 19:14:17 -0500

    add InQuire project to portfolio

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,6 +1,7 @@
 import React from "react";
 import campsiteFinderLogo from "../assets/images/campsite-finder-logo.png";
 import projectBooleanLogo from "../assets/images/project-boolean-logo.png";
+import inQuireLogo from "../assets/images/inquire-logo.png";
 import ProjectCard from "../components/ProjectCard";
 
 function Portfolio() {
@@ -9,6 +10,7 @@ function Portfolio() {
             <h2 className="text-2xl py-2">Portfolio</h2>
 
             <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3">
+                <ProjectCard name="InQuire" deploy="https://inquire-6846.herokuapp.com/" github="https://github.com/damianjuan/Project-3/tree/master" logo={inQuireLogo} />
                 <ProjectCard name="Project Boolean" deploy="https://project-boolean.herokuapp.com/" github="https://github.com/HuhnDaniel/project-boolean/tree/master" logo={projectBooleanLogo} />
                 <ProjectCard name="Campsite Finder" deploy="https://huhndaniel.github.io/campsite-finder/" github="https://github.com/HuhnDaniel/campsite-finder/tree/master" logo={campsiteFinderLogo} />
             </div>
