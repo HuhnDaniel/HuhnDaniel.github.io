966b6c8b138d4d835ce016f1a2eb17fd633b14d5
HuhnDaniel <danielphuhn@gmail.com>
2021-03-05 20:07:40 -0600

    add cornerstone to projects

--- src/assets/data/projects.js
+++ src/assets/data/projects.js
@@ -10,10 +10,32 @@ import inQuireLogo from "../images/inquire-logo.png";
 import inQuireImage1 from "../images/inquire-screenshot-1.png";
 import inQuireImage2 from "../images/inquire-screenshot-2.png";
 import inQuireImage3 from "../images/inquire-screenshot-3.png";
+import cornerstoneLogo from "../images/cornerstone-logo.png";
+import cornerstoneImage1 from "../images/cornerstone-screenshot-1.png";
+import cornerstoneImage2 from "../images/cornerstone-screenshot-2.png";
+import cornerstoneImage3 from "../images/cornerstone-screenshot-3.png";
 
 const projects = [
+	{
+		id: 0,
+		name: "Cornerstone Collaborative",
+		deploy: "http://www.cornerstone-collaborative.com",
+		github: "https://github.com/HuhnDaniel/cornerstone/tree/main",
+		logo: cornerstoneLogo,
+		screenshots: [
+			cornerstoneImage1,
+			cornerstoneImage2,
+			cornerstoneImage3
+		],
+		description:
+			`Cornerstone Art and Craft LLC is a company that strives to connect people who create with those who need things created.
+			Partners of the company have personal profiles to which their projects and disciplines can be added, and potential employers
+			can search to find the perfect candidate for the project they need completed.`,
+		technologies: "Technologies used: HTML, TailwindCSS, JavaScript, SERN Stack, Heroku, JawsDB, Git/Github"
+
+	},
     {
-        id: 0,
+        id: 1,
         name: "InQuire",
         deploy: "https://inquiresurveys.herokuapp.com/home",
         github: "https://github.com/HuhnDaniel/InQuire/tree/master",
@@ -30,7 +52,7 @@ const projects = [
         technologies: "Technologies used: HTML, TailwindCSS, JavaScript, SERN Stack, Heroku, JawsDB, Git/GitHub"
     },
     {
-        id: 1,
+        id: 2,
         name: "Project Boolean",
         deploy: "https://project-boolean.herokuapp.com/",
         github: "https://github.com/HuhnDaniel/project-boolean/tree/master",
@@ -45,7 +67,7 @@ const projects = [
         technologies: "Technologies used: HTML, CSS, JavaScript, NodeJS, Bootstrap, Heroku, JawsDB, Git/GitHub"
     },
     {
-        id: 2,
+        id: 3,
         name: "Campsite Finder",
         deploy: "https://huhndaniel.github.io/campsite-finder/",
         github: "https://github.com/HuhnDaniel/campsite-finder/tree/master",
