013db863aa6cc3b4cfea1da3e3d8c3ef27301acb
HuhnDaniel <danielphuhn@gmail.com>
2021-04-29 18:30:13 -0500

    update resume link to aesthetic resume

--- src/assets/data/resume.js
+++ src/assets/data/resume.js
@@ -1,6 +1,6 @@
 const resume = {
-    view: "https://docs.google.com/document/d/1FAhLfMIWzPaPbqHQfpG83LzAm3j4BUq4rOgi8BgQ1lo/edit?usp=sharing",
-    download: "https://docs.google.com/document/d/1FAhLfMIWzPaPbqHQfpG83LzAm3j4BUq4rOgi8BgQ1lo/export?format=pdf"
+    view: "https://docs.google.com/document/d/19j-KvkI6uaIJDEebSqzEO-3YJGUVlEGFGEGBQ5XAyvM/edit?usp=sharing",
+    download: "https://docs.google.com/document/d/19j-KvkI6uaIJDEebSqzEO-3YJGUVlEGFGEGBQ5XAyvM/export?format=pdf"
 }
 
 export default resume;
